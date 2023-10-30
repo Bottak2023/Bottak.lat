9687ad2fe04e32d8aa0c4a73072782b3a778da9a
raulDeDevWorld <proyection009@gmail.com>
2023-10-30 10:18:18 -0400

    DC

--- next.config.js
+++ next.config.js
@@ -6,6 +6,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  headers: async () => {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+
 }
 
 const withPWA = require('next-pwa')({
@@ -16,20 +31,7 @@ module.exports = withPWA({
   nextConfig
 })
 
-// async headers() {
-//   return [
-//     {
-//       // matching all API routes
-//       source: "/api/:path*",
-//       headers: [
-//         { key: "Access-Control-Allow-Credentials", value: "true" },
-//         { key: "Access-Control-Allow-Origin", value: "*" },
-//         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-//         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-//       ]
-//     }
-//   ]
-// }
+
 
 
 // module.exports = {
