3421ff4df7977435e90b0c5942d0eeea6c09cd62
raulDeDevWorld <proyection009@gmail.com>
2023-10-30 12:46:51 -0400

    DC

--- next.config.js
+++ next.config.js
@@ -15,6 +15,8 @@ const nextConfig = {
       {
         // matching all API routes
         source: "/api/:path*",
+        destination: '/',
+        permanent: true,
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
