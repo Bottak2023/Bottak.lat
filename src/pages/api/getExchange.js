e565e3ff2c88522cfc109da7a89716fae7513cf7
raulDeDevWorld <proyection009@gmail.com>
2023-10-30 15:32:58 -0400

    DC

--- src/pages/api/getExchange.js
+++ src/pages/api/getExchange.js
@@ -7,46 +7,49 @@ export default async function webScraping(req, res) {
     console.log('node')
     console.log(req.body.divisas)
 
-    const browser = await puppeteer.connect({
-        browserWSEndpoint: `wss://chrome.browserless.io?token=3f0d97ad-6529-41ea-8431-2b631bfc983d`,
-    })
-
-    const page = await browser.newPage()
-    await page.setDefaultNavigationTimeout(0);
-
-    await page.goto('https://google.com/')
-
-    const search = await page.waitForSelector('#APjFqb')
 
 
-    await search.type(`exchange divisas`)
-    await page.keyboard.press('Enter')
-    await page.waitForNavigation()
-    await page.click('[class="M2vV3 vOY7J"]')
-
-    console.log(req.body.divisas)
-
-    // if (req.body.divisas !== undefined && req.body.divisas.length > 0) {
 
 
-    const obj = await req.body.divisas.reduce(async (previousPromise, i) => {
-        try {
-            let acc = await previousPromise;
-            const search2 = await page.waitForSelector('#APjFqb')
-            await search2.type(`USD to ${i}`)
-            await page.keyboard.press('Enter')
-            await page.waitForNavigation()
-            const value = await page.evaluate(() => document.querySelector('.SwHCTb').innerText)
-            console.log(value)
-            await page.click('[class="M2vV3 vOY7J"]')
+    if (req.method === "POST") {
 
-            return { ...acc, [i]: value }
-        } catch (error) {
-            console.log(error)
-            return {}
-        }
+        const browser = await puppeteer.connect({
+            browserWSEndpoint: `wss://chrome.browserless.io?token=3f0d97ad-6529-41ea-8431-2b631bfc983d`,
+        })
+        const page = await browser.newPage()
+        await page.setDefaultNavigationTimeout(0);
+        await page.goto('https://google.com/')
+        const search = await page.waitForSelector('#APjFqb')
+        await search.type(`exchange divisas`)
+        await page.keyboard.press('Enter')
+        await page.waitForNavigation()
+        await page.click('[class="M2vV3 vOY7J"]')
 
-    }, Promise.resolve({}));
+        const obj = await req.body.divisas.reduce(async (previousPromise, i) => {
+            try {
+                let acc = await previousPromise;
+                const search2 = await page.waitForSelector('#APjFqb')
+                await search2.type(`USD to ${i}`)
+                await page.keyboard.press('Enter')
+                await page.waitForNavigation()
+                const value = await page.evaluate(() => document.querySelector('.SwHCTb').innerText)
+                console.log(value)
+                await page.click('[class="M2vV3 vOY7J"]')
+
+                return { ...acc, [i]: value }
+            } catch (error) {
+                console.log(error)
+                return {}
+            }
+
+        }, Promise.resolve({}));
+
+        return res.json(obj)
+
+
+    }
+
+
 
 
 
@@ -59,6 +62,5 @@ export default async function webScraping(req, res) {
     //     optionsSuccessStatus: 200,
     //  });
 
-    return res.json(obj)
 }
 
