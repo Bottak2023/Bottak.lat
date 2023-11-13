69d0a2c8c0a7cf3c1ff2dfc5cf9a6308e71e7b4b
raulDeDevWorld <proyection009@gmail.com>
2023-11-12 20:14:27 -0400

    DC

--- src/app/(layout)/(layout-admin)/Admin/Cambios/page.jsx
+++ src/app/(layout)/(layout-admin)/Admin/Cambios/page.jsx
@@ -1,216 +1,213 @@
-'use client'
-
+'use client';
+import { useUser } from '@/context/Context'
+import { getSpecificData, writeUserData } from '@/firebase/database'
+import { useState, useEffect, useRef } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 import Button from '@/components/Button'
-import Subtitle from '@/components/Subtitle'
+import Tag from '@/components/Tag'
+import Loader from '@/components/Loader'
 import Modal from '@/components/Modal'
-import { writeUserData, getSpecificData, removeData } from '@/firebase/database'
+import Select from '@/components/Select'
+import { estado as estadoCONST } from '@/constants/'
 
-// import Select from '@/components/Select'
-import { useUser } from '@/context/Context.js'
+export default function Home() {
 
-// import Tag from '@/components/Tag'
-import { useRouter } from 'next/navigation';
+  const { user, userDB, setUserProfile, modal, setModal, users, setUsers, setUserSuccess, success, setUserData, postsIMG, setUserPostsIMG, divisas, setDivisas, item, setItem, exchange, setExchange, destinatario, setDestinatario } = useUser()
+  const [filter, setFilter] = useState('')
+  const [state, setState] = useState({})
+  const [remesasDB, setRemesasDB] = useState(undefined)
+  const [estado, setEstado] = useState('')
+  const refFirst = useRef(null);
 
-import { WithAuth } from '@/HOCs/WithAuth'
-import { useEffect, useState } from 'react'
-// import { writeUserData, readUserData, updateUserData, deleteUserData } from '@/supabase/utils'
-// import { uploadStorage } from '@/supabase/storage'
-// import LoaderBlack from '@/components/LoaderBlack'
-// import { disponibilidad as dispo} from '@/constants'
-
-function Home() {
-    const { user, userDB, users, setUsers, setUserProfile, setUserSuccess, success, setUserData, postsIMG, setUserPostsIMG, item, setItem, modal, setModal } = useUser()
-
-    const router = useRouter()
-    const [filter, setFilter] = useState('')
-    const [state, setState] = useState({})
-    async function save(i) {
-        // console.log(state[i.uuid]['nombre de producto 1'])
-        // setUserSuccess('Cargando')
-        // await updateUserData('Producto', state[i.uuid], i.uuid)
-        // postImage[i.uuid] && await uploadStorage('Producto', postImage[i.uuid], i.uuid, updateUserData, true)
-        // const obj = { ...state }
-        // delete obj[i.uuid]
-        // setState(obj)
-        // setUserSuccess('')
-        // // state[i.uuid]['nombre de producto 1'] !== undefined && setUserDistributorPDB(undefined)
-        // return  readUserData('Producto', 'Precio-Justo-SRL-Data', setUserDistributorPDB, 'distribuidor')
-    }
-
-
-    function onChangeHandler(e) {
-        setFilter(e.target.value)
-    }
-
-
-    async function deletConfirm() {
-        // await deleteUserData('Producto', item.uuid)
-        // await readUserData('Producto', 'Precio-Justo-SRL-Data', setUserDistributorPDB, 'distribuidor')
-        // setModal(false)
-    }
-
-    async function habilitarConfirm() {
-        writeUserData(`users/${item}/profile`, { habilitado: true }, setUserSuccess)
-        return getSpecificData(`/users/`, setUsers)
-    }
-
-    async function desabilitarConfirm() {
-
-        writeUserData(`users/${item}/profile`, { habilitado: false }, setUserSuccess)
-        return getSpecificData(`/users/`, setUsers)
-
-        // await deleteUserData('Producto', item.uuid)
-        // await readUserData('Producto', 'Precio-Justo-SRL-Data', setUserDistributorPDB, 'distribuidor')
-        // setModal(false)
-    }
-
-    function manage(i, data) {
-        setItem(i)
-        setModal(data)
-        console.log(users[item])
-    }
-
-    function redirect() {
-        router.push('Administrador/Plantilla/Agregar')
-    }
-
-    function sortArray(x, y) {
-        if (x['nombre de producto 1'].toLowerCase() < y['nombre de producto 1'].toLowerCase()) { return -1 }
-        if (x['nombre de producto 1'].toLowerCase() > y['nombre de producto 1'].toLowerCase()) { return 1 }
-        return 0
-    }
-
-    console.log(users)
-    useEffect(() => {
-        users === undefined && getSpecificData(`/users/`, setUsers)
-
-    }, [users])
-
-    return (
-        <div className='h-full'>
-            <div className="relative left-0 h-full overflow-x-auto shadow-md p-5 bg-white min-h-[80vh]">
-                {modal === 'Delete' && <Modal theme="Danger" button="Eliminar" funcion={deletConfirm}>Estas seguro de eliminar al siguiente usuario:  {item['nombre']}</Modal>}
-                {modal === 'Habilitar' && <Modal theme="Primary" button="Habilitar" funcion={habilitarConfirm}>Estas seguro de habilitar al siguiente usuario:  {item['nombre']}</Modal>}
-                {modal === 'Desabilitar' && <Modal theme="Danger" button="Desabilitar" funcion={desabilitarConfirm}>Estas seguro de Desabilitar al siguiente usuario:  {item['nombre']}</Modal>}
-
-                <h3 className='font-medium text-[14px]'>Lista De Usuarios</h3>
-                <br />
-                <input type="text" className='border-b-[1px] text-[14px] outline-none w-[400px]' onChange={onChangeHandler} placeholder='Buscar Usuario' />
-                <br />
-                <br />
-                {/* <div className='min-w-[1900px] flex justify-start items-center my-5 '>
-                <h3 className="flex pr-12 text-[14px]" htmlFor="">Disponibilidad</h3>
-                <div className="grid grid-cols-3 gap-4 w-[500px] ">
-                    <Tag theme={disponibilidad == 'En 24 hrs' ? 'Primary' : 'Secondary'} click={() => setDisponibilidad(disponibilidad == 'En 24 hrs' ? '' : 'En 24 hrs')}>En 24 hrs</Tag>
-                    <Tag theme={disponibilidad == 'Inmediato' ? 'Primary' : 'Secondary'} click={() => setDisponibilidad(disponibilidad == 'Inmediato' ? '' : 'Inmediato')}>Inmediato</Tag>
-                    <Tag theme={disponibilidad == 'No disponible' ? 'Primary' : 'Secondary'} click={() => setDisponibilidad(disponibilidad == 'No disponible' ? '' : 'No disponible')}>No disponible</Tag>
-                </div>
-            </div>
-            <div className='min-w-[1900px] flex justify-start items-center my-5  '>
-                <h3 className="flex pr-12 text-[14px]">Categorias</h3>
-                <div className="grid grid-cols-3 gap-4 w-[500px] " >
-                    <Tag theme={categoria == 'Titanio' ? 'Primary' : 'Secondary'} click={() => setCategoria(categoria == 'Titanio' ? '' : 'Titanio')}>Titanio</Tag>
-                    <Tag theme={categoria == 'Acero' ? 'Primary' : 'Secondary'} click={() => setCategoria(categoria == 'Acero' ? '' : 'Acero')}>Acero</Tag>
-                    <Tag theme={categoria == 'Otros' ? 'Primary' : 'Secondary'} click={() => setCategoria(categoria == 'Otros' ? '' : 'Otros')}>Otros</Tag>
-                </div>
-            </div> */}
-
-                <table className="w-full overflow-visible min-w-[1500px]  text-[14px] text-left text-gray-500 border-t-4 border-gray-400">
-                    <thead className="text-[14px] text-gray-700 uppercase bg-gray-50  ">
-                        <tr>
-                            <th scope="col" className="px-3 py-3">
-                                #
-                            </th>
-                            <th scope="col" className="px-3 py-3">
-                                Nombre
-                            </th>
-                            <th scope="col" className="px-3 py-3">
-                                DNI
-                            </th>
-                            <th scope="col" className="px-3 py-3">
-                                Pais
-                            </th>
-                            <th scope="col" className="px-3 py-3">
-                                Foto 1
-                            </th>
-                            <th scope="col" className="px-3 py-3">
-                                Foto 2
-                            </th>
-                            <th scope="col" className="px-8 py-3">
-                                Foto 3
-                            </th>
-                            <th scope="col" className="px-8 py-3">
-                                WhatsApp
-                            </th>
-                            <th scope="col" className="px-3 py-3">
-                                Habilitar
-                            </th>
-                            <th scope="col" className="px-3 py-3">
-                                Eliminar
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users && users !== undefined && Object.keys(users).map((i, index) => {
-
-                            return users[i].profile['nombre'].includes(filter) && <tr className="bg-white text-[14px] border-b   hover:bg-gray-50 " key={index}>
-                                <td className="px-3 py-4  flex font-semibold text-gray-900 ">
-                                    <span className='h-full flex py-2'>{index + 1}</span>
-                                </td>
-                                <td className="px-3 py-4 font-semibold text-gray-900 ">
-                                    {users[i].profile['nombre']}
-                                </td>
-                                <td className="px-3 py-4 font-semibold text-gray-900 ">
-                                    {users[i].profile['dni']}
-                                </td>
-                                <td className="px-3 py-4 font-semibold text-gray-900 ">
-                                    {users[i].profile['pais']}
-                                </td>
-                                <td className="w-32 p-4">
-                                    <img src={users[i].profile.image1} alt="Apple Watch" />
-                                </td>
-                                <td className="w-32 p-4">
-                                    <img src={users[i].profile.image2} alt="Apple Watch" />
-                                </td>
-                                <td className="w-32 p-4">
-                                    <img src={users[i].profile.image3} alt="Apple Watch" />
-                                </td>
-                                <td className="px-3 py-4 font-semibold text-gray-900 ">
-                                    {users[i].profile['whatsapp']}
-                                </td>
-                                <td className="px-3 py-4">
-                                    {users && users[i].profile['habilitado'] === false
-                                        ? <Button theme={"Primary"} click={() => manage(i, 'Habilitar')}>Habilitar</Button>
-                                        : <Button theme={"Danger"} click={() => manage(i, 'Desabilitar')}>Desabilitar</Button>
-                                    }
-                                </td>
-                                <td className="px-3 py-4">
-
-                                    <Button theme={"Danger"} click={() => manage(i, 'Delete')}>Eliminar</Button>
-
-                                </td>
-                            </tr>
-                        })
-                        }
-                    </tbody>
-                </table>
-
-
-
-
-
-
-
-                {success == 'Cargando' && <LoaderBlack />}
-
-            </div>
-
+  function onChangeFilter(e) {
+    setFilter(e.target.value)
+  }
+  function sortArray(x, y) {
+    if (x['translation']['spa']['common'].toLowerCase() < y['translation']['spa']['common'].toLowerCase()) { return -1 }
+    if (x['translation']['spa']['common'].toLowerCase() > y['translation']['spa']['common'].toLowerCase()) { return 1 }
+    return 0
+  }
+  function handlerSelect(name, i, uuid) {
+    setState({ ...state, [uuid]: { [name]: i } })
+  }
+  function save(uuid) {
+    setModal('Guardando...')
+    writeUserData(`envios/${uuid}`, state[uuid], setUserSuccess, () => { setModal('') })
+  }
+  const prev = () => {
+    requestAnimationFrame(() => {
+      const scrollLeft = refFirst.current.scrollLeft;
+      console.log(scrollLeft)
+      const itemWidth = screen.width - 50
+      refFirst.current.scrollLeft = scrollLeft - itemWidth;
+    });
+  };
+  const next = () => {
+    requestAnimationFrame(() => {
+      const scrollLeft = refFirst.current.scrollLeft;
+      console.log(scrollLeft)
+      const itemWidth = screen.width - 50
+      console.log(itemWidth)
+      refFirst.current.scrollLeft = scrollLeft + itemWidth;
+    });
+  };
+  useEffect(() => {
+    remesasDB === undefined && getSpecificData(`/envios/`, setRemesasDB)
+  }, [remesasDB])
+  return (
+    <main className='w-full h-full'>
+      {modal === 'Guardando...' && <Loader> {modal} </Loader>}
+      {modal === 'Save' && <Modal funcion={saveConfirm}>Estas seguro de modificar la tasa de cambio de:  {item['currency']}</Modal>}
+      {modal === 'Disable' && <Modal funcion={disableConfirm}>Estas seguro de {item.habilitado !== undefined && item.habilitado !== false ? 'DESABILITAR' : 'HABILITAR'} el siguiente item:  {item['currency']}</Modal>}
+      <button className='fixed text-[20px] text-gray-500 h-[50px] w-[50px] rounded-full inline-block left-[0px] top-0 bottom-0 my-auto bg-[#00000010] z-20 lg:left-[20px]' onClick={prev}>{'<'}</button>
+      <button className='fixed text-[20px] text-gray-500 h-[50px] w-[50px] rounded-full inline-block right-[0px] top-0 bottom-0 my-auto bg-[#00000010] z-20 lg:right-[20px]' onClick={next}>{'>'}</button>
+      <div className="w-full   relative h-full overflow-auto shadow-2xl p-5 bg-white min-h-[80vh] scroll-smooth" ref={refFirst}>
+        <h3 className='font-medium text-[14px]'>Remesas</h3>
+        <br />
+        <input type="text" className='border-b-[1px] text-[14px] outline-none w-[400px]' onChange={onChangeFilter} placeholder='Buscar por remitente, destinatario o DNI' />
+        <div className='min-w-[1900px] flex justify-start items-center my-5 '>
+          <h3 className="flex pr-12 text-[14px]" htmlFor="">Estado</h3>
+          <div className="grid grid-cols-4 gap-4 w-[700px] ">
+            {estadoCONST.map((i, index) => {
+              return <Tag theme={estado == i ? 'Primary' : 'Secondary'} click={() => setEstado(estado == i ? '' : i)}>{i}</Tag>
+            })}
+          </div>
         </div>
-
-    )
+        <br />
+        <br />
+        <table className="w-full min-w-[2500px] border-[1px] bg-white text-[14px] text-left text-gray-500 border-t-4 border-t-gray-400">
+          <thead className="text-[14px] text-gray-700 uppercase bg-white">
+            <tr>
+              <th scope="col" className="w-[50px] px-3 py-3">
+                #
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                estado
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Remitente
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                DNI remitente
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Pais remitente
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Destinatario
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                DNI destinatario
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Dirección
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Celular
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Nro de cuenta
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Banco
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Importe
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Divisa <br /> de <br /> envio
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Importe con el <br /> cambio aplicado
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Divisa <br /> de <br /> receptor
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                ID de transaccion
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Fecha
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Actualizar
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {remesasDB && remesasDB !== undefined && Object.values(remesasDB).map((i, index) => {
+              return ((i.destinatario !== undefined && i.destinatario.toLowerCase().includes(filter.toLowerCase())) || 
+              (i.remitente !== undefined && i.remitente.toLowerCase().includes(filter.toLowerCase())) || 
+              (i.dni !== undefined && i.dni.toLowerCase().includes(filter.toLowerCase())) || 
+              (i['dni remitente'] !== undefined && i['dni remitente'].toLowerCase().includes(filter.toLowerCase()))) && 
+              (i.estado !== undefined && i.estado.toLowerCase().includes(estado.toLowerCase())) && 
+              <tr className={`text-[14px] border-b hover:bg-gray-100  ${index % 2 === 0 ? '' : ''} `} key={index}>
+                <td className="px-3 py-4  flex  ">
+                  <span className='h-full flex py-2'>{index + 1}</span>
+                </td>
+                {/* {console.log(i['estado'])} */}
+                <td className="min-w-32 px-3 py-4  ">
+                  <Select arr={['En proceso', 'Transfiriendo', 'Exitoso', 'Rechazado']} name='estado' uuid={i.uuid} defaul={i.estado} click={handlerSelect} />
+                </td>
+                <td className="min-w-32 px-3 py-4  ">
+                  {i['remitente']}
+                </td>
+                <td className="min-w-32 px-3 py-4  ">
+                  {i['dni remitente']}
+                </td>
+                <td className="min-w-32 px-3 py-4  ">
+                  {i['pais remitente']}
+                </td>
+                <td className="min-w-32 px-3 py-4  ">
+                  {i['destinatario']}
+                </td>
+                <td className="min-w-32 p-3">
+                  {i['dni']}
+                </td>
+                <td className="min-w-32 p-3">
+                  {i['direccion']}
+                </td>
+                <td className="min-w-32 p-3">
+                  {i['celular']}
+                </td>
+                <td className="min-w-32 p-3">
+                  {i['cuenta destinatario']}
+                </td>
+                <td className="min-w-32 p-3">
+                  {i['nombre de banco']}
+                </td>
+                <td className="px-3 py-4  ">
+                  {i['importe']}
+                </td>
+                <td className=" p-3">
+                  {i['divisa de envio']}
+                </td>
+                <td className="min-w-32 p-3">
+                  {i['cambio']}
+                </td>
+                <td className=" p-3">
+                  {i['divisa de receptor']}
+                </td>
+                <td className="min-w-32 p-3">
+                  {i['uuid']}
+                </td>
+                <td className="min-w-32 p-3">
+                  {i['fecha']}
+                </td>
+                <td className="px-3 py-4">
+                  {state && state !== undefined && state[i.uuid] && state[i.uuid] !== undefined
+                    ? <Button theme={"Success"} click={() => save(i.uuid)}>Guardar</Button>
+                    : <Button theme={"Disable"}>Desabilitado</Button>
+                  }
+                </td>
+              </tr>
+            })
+            }
+          </tbody>
+        </table>
+      </div>
+    </main>
+  )
 }
 
-
-
-
-export default WithAuth(Home)
--- src/pages/api/getExchange.js
+++ src/pages/api/getExchange.js
@@ -1,54 +1,37 @@
-// import puppeteer from "puppeteer";
 import puppeteer from "puppeteer";
-// import NextCors from 'nextjs-cors';
-
-// import chromium from "chrome-aws-lambda"
 export default async function webScraping(req, res) {
 
-    console.log('node')
-    console.log(req.body.divisas)
-
     if (req.method === "POST") {
-        // const browser = await puppeteer.connect({
-        //     browserWSEndpoint: `wss://chrome.browserless.io?token=3f0d97ad-6529-41ea-8431-2b631bfc983d`,
-        // })
 
-        const browser = await puppeteer.launch({headless: 'new'});
+        // const browser = await puppeteer.launch({headless: 'new'});
 
-        const page = await browser.newPage()
-        await page.setDefaultNavigationTimeout(0);
-        await page.goto('https://google.com/')
-        const search = await page.waitForSelector('#APjFqb')
-        await search.type(`exchange divisas`)
-        await page.keyboard.press('Enter')
-        await page.waitForNavigation()
-        await page.click('[class="M2vV3 vOY7J"]')
+        // const page = await browser.newPage()
+        // await page.setDefaultNavigationTimeout(0);
+        // await page.goto('https://google.com/')
+        // const search = await page.waitForSelector('#APjFqb')
+        // await search.type(`exchange divisas`)
+        // await page.keyboard.press('Enter')
+        // await page.waitForNavigation()
+        // await page.click('[class="M2vV3 vOY7J"]')
 
-        const obj = await req.body.divisas.reduce(async (previousPromise, i) => {
-            try {
-                let acc = await previousPromise;
-                const search2 = await page.waitForSelector('#APjFqb')
-                await search2.type(`USD to ${i}`)
-                await page.keyboard.press('Enter')
-                await page.waitForNavigation()
-                const value = await page.evaluate(() => document.querySelector('.SwHCTb').innerText)
-                console.log(value)
-                await page.click('[class="M2vV3 vOY7J"]')
+        // const obj = await req.body.divisas.reduce(async (previousPromise, i) => {
+        //     try {
+        //         let acc = await previousPromise;
+        //         const search2 = await page.waitForSelector('#APjFqb')
+        //         await search2.type(`USD to ${i}`)
+        //         await page.keyboard.press('Enter')
+        //         await page.waitForNavigation()
+        //         const value = await page.evaluate(() => document.querySelector('.SwHCTb').innerText)
+        //         console.log(value)
+        //         await page.click('[class="M2vV3 vOY7J"]')
 
-                return { ...acc, [i]: value }
-            } catch (error) {
-                console.log(error)
-                return {}
-            }
-        }, Promise.resolve({}));
-        await browser.close()
-        return res.json({...obj, USD: '1 USD'})
+        //         return { ...acc, [i]: value }
+        //     } catch (error) {
+        //         console.log(error)
+        //         return {}
+        //     }
+        // }, Promise.resolve({}));
+        // await browser.close()
+        // return res.json({...obj, USD: '1 USD'})
     }
 }
-
-        // await NextCors(req, res, {
-        //     // Options
-        //     methods: ['GET',  'POST',],
-        //     origin: '*',
-        //     optionsSuccessStatus: 200,
-        // });
