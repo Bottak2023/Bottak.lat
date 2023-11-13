66f6d10a28e85ad90fbb0ea3936797121b9c51f5
raulDeDevWorld <proyection009@gmail.com>
2023-11-13 03:18:24 -0400

    DC

--- src/app/(layout)/(layout-admin)/Admin/page.jsx
+++ src/app/(layout)/(layout-admin)/Admin/page.jsx
@@ -34,7 +34,7 @@ const cardsDB = [
 function Card({i}) {
   
   return <Link href={i.url} >
-    <div className='w-full sm:min-w-[300px] sm:max-w-[500px] bg-[#7700ff] flex flex-col justify-center items-center rounded-[20px] p-5 mb-[20px]'>
+    <div className='w-full sm:min-w-[300px] sm:max-w-[500px] bg-[#7700ff] flex flex-col justify-center items-center rounded-[20px] p-5 mb-[40px] md:mb-0'>
       <img src={i.img} className='block h-[100px] mb-5' alt={i.title} />
       <Button theme='Primary' >{i.title}</Button>
     </div>
@@ -46,7 +46,7 @@ export default function Home() {
 
   return (
     <main className='w-full  relative flex flex-col justify-center items-center '>
-      <div className='w-full   min-h-full space-y-12 md:space-y-0 md:grid md:grid-cols-2 md:gap-10 lg:grid-cols-3   '>
+      <div className='w-full   min-h-full md:grid md:grid-cols-2 md:gap-10 lg:grid-cols-3   '>
         {
           cardsDB.map((i) => <Card i={i} key={i.title} />)
         }
