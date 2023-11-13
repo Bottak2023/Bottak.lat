0b83af50151fb8742ce8e935e220e7f780c13952
raulDeDevWorld <proyection009@gmail.com>
2023-11-13 03:47:16 -0400

    DC

--- src/app/(layout)/Destinatarios/page.jsx
+++ src/app/(layout)/Destinatarios/page.jsx
@@ -82,9 +82,8 @@ export default function Home() {
             <div className="w-full   relative h-full overflow-auto shadow-2xl p-5 bg-white min-h-[80vh] scroll-smooth" ref={refFirst}>                <h3 className='font-medium text-[14px]'>Destinatarios</h3>
                 <br />
                 <input type="text" className='border-b-[1px] text-[14px] outline-none w-[400px]' onChange={onChangeFilter} placeholder='Buscar Destinatario' />
-                <br />
-                <br />
-                <div className=''>
+                
+                <div className='left-[50px]'>
                     <div className='flex  justify-center items-center h-[50px] text-white text-[14px] font-medium bg-[#32CD32] border border-gray-200 rounded-[10px] px-10 cursor-pointer mr-2' onClick={redirect}>Nuevo destinatario</div>
                     <div className='md:flex hidden justify-center items-center bg-[#0064FA] h-[50px] w-[50px]  rounded-full text-white cursor-pointer' onClick={redirect}> <span className='text-white text-[30px]'>+</span> </div>
                 </div>
