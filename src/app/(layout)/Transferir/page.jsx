795e4748f29b56146bb4dae988e072cba665036c
raulDeDevWorld <proyection009@gmail.com>
2023-12-15 12:32:24 -0400

    DC

--- src/app/(layout)/Transferir/page.jsx
+++ src/app/(layout)/Transferir/page.jsx
@@ -19,13 +19,13 @@ import { getSpecificDataEq, getSpecificData, writeUserData, removeData } from '@
 
 import Link from 'next/link'
 function Home() {
-    
+
     const { nav, setNav, user, userDB, setUserProfile, select, setDestinatario, success, setUserData, postsIMG, setUserPostsIMG, isSelect3, setIsSelect3, isSelect4, setIsSelect4, modal, setModal, destinatario, qr, setQr, QRurl, setQRurl, countries, setEnviosDB, setCambiosDB } = useUser()
     const router = useRouter()
-      
+
     const [postImage, setPostImage] = useState(undefined)
     const [urlPostImage, setUrlPostImage] = useState(undefined)
-      
+
     function onChangeHandler(e) {
         setDestinatario({ ...destinatario, [e.target.name]: e.target.value })
     }
@@ -94,20 +94,47 @@ function Home() {
                 <div className='lg:hidden'>
                     <h3 className='text-center pb-3  text-green-400 lg:hidden'>QR y cuenta para deposito Bancario</h3>
                 </div>
+
+
+
                 <div className=' space-y-5'>
+                    <Label htmlFor="">Elige una banco para deposito QR</Label>
+                    <SelectBank name="nombre de banco" propHandlerIsSelect={handlerIsSelect4} propIsSelect={isSelect4} operation="envio" click={handlerBankSelect} arr={Object.values(countries[userDB.cca3].countries)} />
+                </div>
+                {/* <div className=' space-y-5'>
+                <Label htmlFor="">Numero de cuenta transferidora</Label>
+                <Input type="text" name="cuenta transferidora" onChange={onChangeHandler} required />
+            </div> */}
+                {/* <div className=' space-y-5'>
+                <Label htmlFor="">Titular de banco de transferencia</Label>
+                <Input type="text" name="titular de banco" onChange={onChangeHandler} required />
+            </div> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                {destinatario['banco de transferencia'] !== undefined && <div className=' space-y-5'>
                     <Label htmlFor="">QR bancario</Label>
                     <Link href={countries[userDB.cca3].url ? countries[userDB.cca3].url : ''} className="w-full flex flex-col justify-center items-center" download >
                         <label className=" flex flex-col justify-start items-center w-[200px] h-[230px] bg-white border border-gray-300 text-gray-900 text-[12px]  focus:ring-blue-500 focus:border-blue-500 rounded-[10px]" >
-                            {countries && countries[userDB.cca3] && countries[userDB.cca3].countries !== undefined && countries[userDB.cca3].countries[userDB.banco] !== undefined ? <img className=" flex justify-center items-center w-[200px] h-[200px] bg-white border border-gray-300 text-gray-900 text-[12px]  focus:ring-blue-500 focus:border-blue-500 rounded-[10px]" style={{ objectPosition: 'center' }} src={countries[userDB.cca3].countries[userDB.banco] !== undefined ? countries[userDB.cca3].countries[userDB.banco].qrURL : ''} alt="" />
+                            {countries && countries[userDB.cca3] && countries[userDB.cca3].countries !== undefined && countries[userDB.cca3].countries[destinatario['banco de transferencia'] ] !== undefined ? <img className=" flex justify-center items-center w-[200px] h-[200px] bg-white border border-gray-300 text-gray-900 text-[12px]  focus:ring-blue-500 focus:border-blue-500 rounded-[10px]" style={{ objectPosition: 'center' }} src={countries[userDB.cca3].countries[destinatario['banco de transferencia'] ] !== undefined ? countries[userDB.cca3].countries[destinatario['banco de transferencia'] ].qrURL : ''} alt="" />
                                 : 'QR no disponible'}
                             {destinatario && destinatario.importe} {destinatario && destinatario['divisa de envio']}
                         </label>
                     </Link>
-
-                    <span className="block text-white text-center" >Cta. {userDB !== undefined && userDB.cca3 !== undefined && userDB.banco !== undefined && countries && countries !== undefined && countries[userDB.cca3] !== undefined && countries[userDB.cca3].countries !== undefined && countries[userDB.cca3].countries[userDB.banco] !== undefined && countries[userDB.cca3].countries[userDB.banco]['cta bancaria']} <br />
-                        {countries && countries !== undefined && countries[userDB.cca3] !== undefined && countries[userDB.cca3].countries[userDB.banco].banco}</span>
-
-                </div>
+                    <span className="block text-white text-center" >Cta. {countries && countries !== undefined && countries[userDB.cca3] !== undefined && countries[userDB.cca3].countries !== undefined && countries[userDB.cca3].countries[destinatario['banco de transferencia'] ] !== undefined && countries[userDB.cca3].countries[destinatario['banco de transferencia']]['cta bancaria']} <br />
+                        {countries && countries !== undefined && countries[userDB.cca3] !== undefined && countries[userDB.cca3].countries[destinatario['banco de transferencia'] ].banco}</span>
+                </div>}
                 {/* <div className=' space-y-5'>
                 <Label htmlFor="">Cuenta bancaria</Label>
                 <span className="block text-white text-center" >{countries && countries !== undefined && countries[userDB.cca3]['cuenta de cobro'] !== undefined && countries[userDB.cca3]['cuenta de cobro']} <br />
